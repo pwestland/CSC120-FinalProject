f0894e968268b411c6d8a0784b627d4df68c8beb
pwestland <pwestland@smith.edu>
2024-12-16 13:19:04 -0500

    house

--- house.java
+++ house.java
@@ -3,12 +3,32 @@ import java.util.ArrayList;
 public class House {
 
     private String description;
-    private boolean locked;
-    private int nRooms; 
-    private ArrayList<RoomList> rooms;
+    private boolean isLocked;
+    private ArrayList<String> rooms;
     
-}
-    public House(String description, boolean locked, int nRooms, ){
-        
+    /* creates the house */
 
+    public House(String description, boolean isLocked, int nRooms){
+        this.rooms = new ArrayList<>();
     }
+
+    /* returns the description of the house */
+
+    public String description(){
+        System.out.println("You are inside of a house.");
+    }
+
+    /* returns whether the house's door to the outside is locked */
+
+    public boolean isLocked(){
+        return this.isLocked;
+    }
+
+    /* returns the number of rooms in the house */
+
+    public int nRooms(){
+        return this.rooms.size();
+    }
+
+}
+
